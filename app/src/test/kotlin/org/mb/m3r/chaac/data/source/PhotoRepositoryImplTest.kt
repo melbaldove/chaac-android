b2fb89bc358455029f9181cdb702645e1f81964d
Melby <melqbaldove@gmail.com>
2017-09-15 16:53:53 +0800

    Add some tests     *PhotoRepositoryImpl - savePhoto() method     *LocalPhotoDataSource - savePhoto() method

---
+++ app/src/test/kotlin/org/mb/m3r/chaac/data/source/PhotoRepositoryImplTest.kt
@@ -0,0 +1,38 @@
+package org.mb.m3r.chaac.data.source
+
+import org.junit.Before
+import org.junit.Test
+import org.mb.m3r.chaac.data.Photo
+import org.mb.m3r.chaac.data.source.local.LocalPhotoDataSource
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import org.mockito.invocation.InvocationOnMock
+import org.junit.Assert.*
+
+/**
+ * @author Melby Baldove
+ * melqbaldove@gmail.com
+ */
+class PhotoRepositoryImplTest {
+    lateinit var localDataSource: PhotoRepository
+    lateinit var photoRepository: PhotoRepository
+
+    @Before
+    fun setup() {
+        localDataSource = mock(LocalPhotoDataSource::class.java)
+        photoRepository = PhotoRepositoryImpl(localDataSource)
+    }
+
+    @Test
+    fun savePhoto() {
+        val mockPhoto = Photo(checksum = "someRandomString", path = "somePath", caption = null)
+        val mockDb = mutableListOf<Photo>()
+        `when`(localDataSource.savePhoto(mockPhoto)).then { invocation: InvocationOnMock? ->
+            mockDb.add(invocation!!.arguments.first() as Photo)
+        }
+        photoRepository.savePhoto(mockPhoto)
+
+        assertTrue(mockDb.contains(mockPhoto))
+    }
+
+}
