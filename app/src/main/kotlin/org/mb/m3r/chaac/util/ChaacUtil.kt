4a984c9b8d8fd385a4ccb10e1b426b35f084d10b
Melby Baldove <melqbaldove@gmail.com>
2017-09-18 11:46:32 +0800

    Implement checksum function
        * Tests
        * Library dependencies

--- app/src/main/kotlin/org/mb/m3r/chaac/util/ChaacUtil.kt
+++ app/src/main/kotlin/org/mb/m3r/chaac/util/ChaacUtil.kt
@@ -3,8 +3,14 @@ package org.mb.m3r.chaac.util
 import android.os.Environment.DIRECTORY_PICTURES
 import android.os.Environment.getExternalStoragePublicDirectory
 import android.util.Log
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import org.apache.commons.codec.binary.Hex
+import org.apache.commons.codec.digest.DigestUtils
 import org.apache.commons.io.FileUtils
+import org.apache.commons.io.IOUtils
 import java.io.File
+import java.io.FileInputStream
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -38,21 +44,49 @@ object ChaacUtil {
         return image
     }
 
-    fun saveImageFromTempFile(path: String): File {
-        val source = File(path)
-        // Create an image file name
+    fun createImageFile(): File {
         val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
         val imageFileName = "pic$timeStamp.jpg"
-        val dest = File(getPictureDirectory(), imageFileName)
 
+        return File(getPictureDirectory(), imageFileName)
+    }
+
+    /*
+     * Stores image from temp directory to picture directory
+     */
+    fun storeImage(path: String): File {
+        val tempSource = File(path)
+        // Create an image file name
+        val dest = createImageFile()
+        moveFile(tempSource, dest)
+
+        return dest
+    }
+
+    fun moveFile(source: File, dest: File) {
         try {
             FileUtils.copyFile(source, dest)
             source.delete()
         } catch (e: IOException) {
             Log.e("Error", e.message)
         }
-
-        return dest
     }
+
+    fun checkSum(file: File): Single<String> = Single.create<String> { subscriber ->
+        val fileInputStream = FileInputStream(file)
+        try {
+            /*
+             * This is a workaround to a bug described in this link
+             * https://stackoverflow.com/questions/9126567/method-not-found-using-digestutils-in-android
+             */
+            val checksum = String(Hex.encodeHex(DigestUtils.md5(IOUtils.toByteArray(fileInputStream))))
+            subscriber.onSuccess(checksum)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            subscriber.onError(e)
+        } finally {
+            IOUtils.closeQuietly(fileInputStream)
+        }
+    }.subscribeOn(Schedulers.io())
 
 }
