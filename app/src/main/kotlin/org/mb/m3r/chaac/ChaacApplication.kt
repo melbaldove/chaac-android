ad768b35283bfd2999671cf7106ec77956794c2b
Melby Baldove <melqbaldove@gmail.com>
2017-09-28 15:47:16 +0800

    Setup LeakCanary

--- app/src/main/kotlin/org/mb/m3r/chaac/ChaacApplication.kt
+++ app/src/main/kotlin/org/mb/m3r/chaac/ChaacApplication.kt
@@ -1,6 +1,7 @@
 package org.mb.m3r.chaac
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 import org.mb.m3r.chaac.di.ApplicationComponent
 import org.mb.m3r.chaac.di.DaggerApplicationComponent
 
@@ -13,6 +14,12 @@ class ChaacApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
         init()
     }
 
